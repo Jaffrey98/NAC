930e96ebc6765f67908a193a728678250a041f45
AkashG6 <akashgarule@gmail.com>
2018-03-30 22:11:04 +0530

    add file handling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
+var fs = require('fs');
+
 // const db = require('./modules/db.js');
 
 app.use(express.static(__dirname + '/client'));
@@ -12,7 +14,59 @@ app.get('/', function(req, res) {
 
 
 io.on('connection', function(socket) {
-   console.log('A user connected');
+   // console.log('A user connected');
+
+   var content;
+   var write = "";
+   var put = "";
+   var network = "localnet1" // later comes from client
+
+   // first read
+   fs.readFile('acl.txt', function (err, data) {
+		content = data.toString().split("\r\n");
+		console.log("content: \n"+content)
+	});
+
+   socket.on('toggle',function(text){
+   		write = ""
+   		console.log("text: "+text)
+   		if (text=="enable"){
+   			put = "allow"
+   		}
+   		else{
+   			put = "deny"
+   		}
+
+   		fs.readFile('acl.txt', function (err, data) {
+   			content = data.toString().split("\r\n");
+   		console.log(content)
+
+   		for (i = 0; i < content.length; i++) {
+		    var words = content[i].split(" ");
+		    if (words[0]==network) {
+		    	write = write + words[0] + " " + put + "\r\n"
+		    }
+		    else if (i < content.length-1){
+		    	write = write + content[i] + "\r\n"
+		    }
+		    else{
+		    	write = write + content[i]
+		    }
+		    console.log("in for")
+		    console.log("write: "+ write)
+		}
+		console.log("write: "+ write)
+
+		 fs.writeFile('acl.txt',write, function(err) {
+		      if (err) {
+		         return console.error(err);
+		      }
+		      
+		   });
+   		});
+
+
+   });
 
    socket.on('disconnect', function () {
       console.log('A user disconnected');
