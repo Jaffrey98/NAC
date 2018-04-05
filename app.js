8fc771e639c7b7fdcf0c6e05f669d57d99a9f09b
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-04-05 19:46:46 +0530

    complete app with feedback based on operation performed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 var exec = require('child_process').exec;
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/net-access-control/www'));
 
 app.get('/', function (req, res) {
 	res.sendFile('index.html');
@@ -20,18 +20,20 @@ io.on('connection', function (socket) {
 	var content;
 	var write = "";
 	var put = "";
-	var network = "localnet" // later comes from client
+
 
 	// first read
 	fs.readFile('F:/Squid/etc/squid/squid.conf', function (err, data) {
 		content = data.toString().split("\r\n");
-		console.log("content: \n" + content)
+		// console.log("content: \n" + content)
 	});
 
-	socket.on('toggle', function (text) {
+	socket.on('toggle', function (rec_data) {
+		// var data = data;
+		var network = rec_data.labName; // later comes from client
 		write = ""
-		console.log("text: " + text)
-		if (text == "enable") {
+		console.log("status: " + rec_data.status)
+		if (rec_data.status == "enable") {
 			put = "allow"
 		} else {
 			put = "deny"
@@ -39,34 +41,35 @@ io.on('connection', function (socket) {
 
 		fs.readFile('F:/Squid/etc/squid/squid.conf', function (err, data) {
 			content = data.toString().split("\r\n");
-			console.log(content)
+			// console.log(content)
 
 			for (i = 0; i < content.length; i++) {
 				var words = content[i].split(" ");
 				if (words[2] == network) {
 					write = write + words[0] + " " + put + " " + network + "\r\n"
+					// console.log("write: " + write)
+
 				} else if (i < content.length - 1) {
 					write = write + content[i] + "\r\n"
 				} else {
 					write = write + content[i]
 				}
-				console.log("in for")
-				console.log("write: " + write)
+				// console.log("in for")
 			}
 			console.log("write: " + write)
 
 			fs.writeFile('F:/Squid/etc/squid/squid.conf', write, function (err) {
 				if (err) {
 					return console.error(err);
+					socket.emit('reply', 'fail')
 				} else {
 					exec('net stop "Squid for Windows" && net start "Squid for Windows"', function (error, stdout, stderr) {
 						console.log(stdout);
+						console.log("after squid restart: "+rec_data.status);
+						socket.emit('reply', rec_data.status);
 					});
-				}
 
-				// exec('net start "Squid for Windows"', function(error, stdout, stderr) {
-				//     console.log(stdout);
-				// });
+				}
 
 			});
 		});
